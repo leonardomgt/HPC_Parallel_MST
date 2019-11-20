626e710e2ff6a6af9979f8d63ae564587e11d448
Leonardo Teixeira <leogt-15@hotmail.com>
2019-11-20 21:21:49 +0100

    script changes

--- bench_das4.sh
+++ bench_das4.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-sbatch --nodes=1 main.job /var/scratch/hpcl1918/mouse_gene.mtx
-sbatch --nodes=2 main.job /var/scratch/hpcl1918/mouse_gene.mtx
-sbatch --nodes=4 main.job /var/scratch/hpcl1918/mouse_gene.mtx
-sbatch --nodes=8 main.job /var/scratch/hpcl1918/mouse_gene.mtx
-sbatch --nodes=16 main.job /var/scratch/hpcl1918/mouse_gene.mtx
+sbatch --nodes=1 main.job /var/scratch/hpcl1918/mouse_gene/mouse_gene.mtx
+sbatch --nodes=2 main.job /var/scratch/hpcl1918/mouse_gene/mouse_gene.mtx
+sbatch --nodes=4 main.job /var/scratch/hpcl1918/mouse_gene/mouse_gene.mtx
+sbatch --nodes=8 main.job /var/scratch/hpcl1918/mouse_gene/mouse_gene.mtx
+sbatch --nodes=16 main.job /var/scratch/hpcl1918/mouse_gene/mouse_gene.mtx
 
-sbatch --nodes=1 main.job /var/scratch/hpcl1918/ldoor.mtx
-sbatch --nodes=2 main.job /var/scratch/hpcl1918/ldoor.mtx
-sbatch --nodes=4 main.job /var/scratch/hpcl1918/ldoor.mtx
-sbatch --nodes=8 main.job /var/scratch/hpcl1918/ldoor.mtx
-sbatch --nodes=16 main.job /var/scratch/hpcl1918/ldoor.mtx
+sbatch --nodes=1 main.job /var/scratch/hpcl1918/ldoor/ldoor.mtx
+sbatch --nodes=2 main.job /var/scratch/hpcl1918/ldoor/ldoor.mtx
+sbatch --nodes=4 main.job /var/scratch/hpcl1918/ldoor/ldoor.mtx
+sbatch --nodes=8 main.job /var/scratch/hpcl1918/ldoor/ldoor.mtx
+sbatch --nodes=16 main.job /var/scratch/hpcl1918/ldoor/ldoor.mtx
 
-sbatch --nodes=1 main.job /var/scratch/hpcl1918/nlpkkt240.mtx
-sbatch --nodes=2 main.job /var/scratch/hpcl1918/nlpkkt240.mtx
-sbatch --nodes=4 main.job /var/scratch/hpcl1918/nlpkkt240.mtx
-sbatch --nodes=8 main.job /var/scratch/hpcl1918/nlpkkt240.mtx
-sbatch --nodes=16 main.job /var/scratch/hpcl1918/nlpkkt240.mtx
+sbatch --nodes=1 main.job /var/scratch/hpcl1918/nlpkkt240/nlpkkt240.mtx
+sbatch --nodes=2 main.job /var/scratch/hpcl1918/nlpkkt240/nlpkkt240.mtx
+sbatch --nodes=4 main.job /var/scratch/hpcl1918/nlpkkt240/nlpkkt240.mtx
+sbatch --nodes=8 main.job /var/scratch/hpcl1918/nlpkkt240/nlpkkt240.mtx
+sbatch --nodes=16 main.job /var/scratch/hpcl1918/nlpkkt240/nlpkkt240.mtx
 
 squeue
